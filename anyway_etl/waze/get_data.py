1216fc327b0953c69babc9110fce197dda48cc7a
carmelp16 <16carmel@gmail.com>
2021-08-13 00:49:29 +0300

    No need for pprint

--- anyway_etl/waze/get_data.py
+++ anyway_etl/waze/get_data.py
@@ -1,14 +1,11 @@
 import requests
-from pprint import pprint
 
 from anyway_etl.waze.config import API_URL, API_PARAMS
 
 
 def get_data_from_api() -> dict:
-    response = requests.get(url=API_PARAMS, params=API_PARAMS, verify=False)
+    response = requests.get(url=API_URL, params=API_PARAMS, verify=False)
 
     response_json = response.json()
-
-    pprint(response_json)
 
     return response_json
