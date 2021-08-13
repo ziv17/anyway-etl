fa5ba12faf8db9e6d7542faefb2567de0c3acc48
carmelp16 <16carmel@gmail.com>
2021-08-13 15:43:00 +0300

    Updated get_data file

--- anyway_etl/waze/get_data.py
+++ anyway_etl/waze/get_data.py
@@ -1,11 +1,19 @@
 import requests
 
-from anyway_etl.waze.config import API_URL, API_PARAMS
+from anyway_etl.waze.utils import WazeDataRetriever, WazeDataFlowsHandler
 
 
-def get_data_from_api() -> dict:
-    response = requests.get(url=API_URL, params=API_PARAMS, verify=False)
+def get_waze_data() -> dict:
+    data_retriever, data_flows_handler = WazeDataRetriever(), WazeDataFlowsHandler()
 
-    response_json = response.json()
+    waze_data = data_retriever.get_data()
 
-    return response_json
+    data_flows = data_flows_handler.get_data_flows(waze_data)
+
+    results = [data_flow.process() for data_flow in data_flows]
+
+    print()
+
+
+if __name__ == "__main__":
+    get_waze_data()
