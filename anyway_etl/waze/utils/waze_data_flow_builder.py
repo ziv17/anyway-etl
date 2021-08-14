39ecf424b65be7232eacf01123ab15630250ca94
carmelp16 <16carmel@gmail.com>
2021-08-14 23:50:47 +0300

    Updated data flow builder

--- anyway_etl/waze/utils/waze_data_flow_builder.py
+++ anyway_etl/waze/utils/waze_data_flow_builder.py
@@ -7,16 +7,13 @@ class WazeDataFlowBuilder:
     def __init__(self):
         self.__parser_retriever = WazeParserRetriever()
 
-    def __yield_items(self, waze_data: dict, field: str):
-        waze_items = waze_data.get(field, [])
-
-        for waze_item in waze_items:
-            yield waze_item
+    def __get_items(self, waze_data: dict, field: str):
+        return waze_data.get(field, [])
 
     def build_data_flow(self, waze_data: dict, field: str) -> DF.Flow:
 
         get_parser = self.__parser_retriever.get_parser
 
-        return DF.Flow(
-            self.__yield_items(waze_data, field), DF.parallelize(get_parser(field))
-        )
+        parser = get_parser(field)
+
+        return DF.Flow(self.__get_items(waze_data, field), parser)
