5bcd2302cc8bdbf365f69e79cc212fa1bc1d604d
Carmel <16carmel@gmail.com>
2021-08-18 23:14:32 +0300

    Build jams data flow

--- anyway_etl/waze/utils/waze_data_flows_handler.py
+++ anyway_etl/waze/utils/waze_data_flows_handler.py
@@ -3,7 +3,7 @@ from .waze_data_flow_builder import WazeDataFlowBuilder
 
 class WazeDataFlowsHandler:
     def __init__(self):
-        self.__fields = ["alerts"]
+        self.__fields = ["alerts", "jams"]
         self.__data_flow_builder = WazeDataFlowBuilder()
 
     def get_data_flows(self, waze_data: dict) -> list:
