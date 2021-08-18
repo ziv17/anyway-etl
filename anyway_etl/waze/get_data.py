42302c847ada43120780c0f1ca77c0048169f090
Carmel <16carmel@gmail.com>
2021-08-18 23:25:52 +0300

    Neglect parallelization for now...

--- anyway_etl/waze/get_data.py
+++ anyway_etl/waze/get_data.py
@@ -1,11 +1,4 @@
-import dataflows as DF
-import multiprocessing as mp
-
 from anyway_etl.waze.utils import WazeDataRetriever, WazeDataFlowsHandler
-
-
-def _process_dataflow(dataflow: DF):
-    dataflow.process()
 
 
 def get_waze_data() -> dict:
@@ -15,8 +8,8 @@ def get_waze_data() -> dict:
 
     dataflows = dataflows_handler.get_data_flows(waze_data)
 
-    with mp.Pool(mp.cpu_count()) as pool:
-        pool.map(_process_dataflow, dataflows)
+    for dataflow in dataflows:
+        dataflow.process()
 
 
 if __name__ == "__main__":
