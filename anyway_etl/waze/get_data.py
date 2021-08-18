72371791437725e7779dba0ea30d15a0748a3771
Carmel <16carmel@gmail.com>
2021-08-18 23:23:44 +0300

    Process multiple dataflows in parallel

--- anyway_etl/waze/get_data.py
+++ anyway_etl/waze/get_data.py
@@ -1,15 +1,22 @@
+import dataflows as DF
+import multiprocessing as mp
+
 from anyway_etl.waze.utils import WazeDataRetriever, WazeDataFlowsHandler
 
 
+def _process_dataflow(dataflow: DF):
+    dataflow.process()
+
+
 def get_waze_data() -> dict:
-    data_retriever, data_flows_handler = WazeDataRetriever(), WazeDataFlowsHandler()
+    data_retriever, dataflows_handler = WazeDataRetriever(), WazeDataFlowsHandler()
 
     waze_data = data_retriever.get_data()
 
-    data_flows = data_flows_handler.get_data_flows(waze_data)
+    dataflows = dataflows_handler.get_data_flows(waze_data)
 
-    for data_flow in data_flows:
-        data_flow.proccess()
+    with mp.Pool(mp.cpu_count()) as pool:
+        pool.map(_process_dataflow, dataflows)
 
 
 if __name__ == "__main__":
