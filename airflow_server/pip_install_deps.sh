c8ba3ca1515676f3bdf4cb5735b0e9c13ec4608c
Ori Hoch <ori@uumpa.com>
2021-08-22 09:01:07 +0300

    add support for running against a different anyway-etl branch

--- airflow_server/pip_install_deps.sh
+++ airflow_server/pip_install_deps.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
-NEW_ANYWAY_ETL_COMMIT="$(curl -fs https://api.github.com/repos/hasadna/anyway-etl/branches/main | jq -r .commit.sha)"
+ANYWAY_ETL_BRANCH="${ANYWAY_ETL_BRANCH:-main}"
+
+NEW_ANYWAY_ETL_COMMIT="$(curl -fs https://api.github.com/repos/hasadna/anyway-etl/branches/${ANYWAY_ETL_BRANCH} | jq -r .commit.sha)"
 if expr length "${NEW_ANYWAY_ETL_COMMIT}" '>' 10; then
   HAS_NEW_ANYWAY_ETL_COMMIT="yes"
 else
@@ -18,10 +20,10 @@ else
 fi
 if [ "${HAS_OLD_ANYWAY_ETL_COMMIT}" == "no" ] || [ "${HAS_NEW_ANYWAY_ETL_COMMIT}" == "no" ] || [ "${OLD_ANYWAY_ETL_COMMIT}" != "${NEW_ANYWAY_ETL_COMMIT}" ]; then
   echo Updating anyway-etl dependencies... &&\
-  ANYWAY_COMMIT="$(curl -s "https://raw.githubusercontent.com/hasadna/anyway-etl/main/anyway-${ANYWAY_BRANCH:-dev}-commit.txt")" &&\
-  "${ANYWAY_ETL_VENV}/bin/pip" install --upgrade -qqr "https://raw.githubusercontent.com/hasadna/anyway-etl/main/requirements.txt" &&\
+  ANYWAY_COMMIT="$(curl -s "https://raw.githubusercontent.com/hasadna/anyway-etl/${ANYWAY_ETL_BRANCH}/anyway-${ANYWAY_BRANCH:-dev}-commit.txt")" &&\
+  "${ANYWAY_ETL_VENV}/bin/pip" install --upgrade -qqr "https://raw.githubusercontent.com/hasadna/anyway-etl/${ANYWAY_ETL_BRANCH}/requirements.txt" &&\
   "${ANYWAY_ETL_VENV}/bin/pip" install --upgrade -qqe "git+https://github.com/hasadna/anyway@${ANYWAY_COMMIT}#egg=anyway" &&\
-  "${ANYWAY_ETL_VENV}/bin/pip" install --upgrade -qqe "git+https://github.com/hasadna/anyway-etl@main#egg=anyway-etl" &&\
+  "${ANYWAY_ETL_VENV}/bin/pip" install --upgrade -qqe "git+https://github.com/hasadna/anyway-etl@${ANYWAY_ETL_BRANCH}#egg=anyway-etl" &&\
   if [ "${HAS_NEW_ANYWAY_ETL_COMMIT}" == "yes" ]; then
     echo "${NEW_ANYWAY_ETL_COMMIT}" > "${ANYWAY_ETL_VENV}/anyway_etl_commit.txt"
   fi &&\
