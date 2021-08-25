829595f845d4dce6301a8727d037b7724bd86048
Carmel <16carmel@gmail.com>
2021-08-25 12:37:25 +0300

    Dump the results as CSV

--- anyway_etl/waze/utils/dataflow_builder.py
+++ anyway_etl/waze/utils/dataflow_builder.py
@@ -1,9 +1,14 @@
+import os
 import dataflows as DF
 
+
 class DataflowBuilder:
-    def __init__(self, parser_retriever, importer_handler):
+    def __init__(self, parser_retriever):
         self.parser_retriever = parser_retriever
-        self.importer_handler = importer_handler
+
+    @property
+    def parent_directory(self):
+        return os.path.dirname(os.path.dirname(__file__))
 
     def get_items(self, waze_data: dict, field: str) -> list:
         return waze_data.get(field, [])
@@ -11,10 +16,8 @@ class DataflowBuilder:
     def build_dataflow(self, waze_data: dict, field: str) -> DF.Flow:
         parser = self.parser_retriever.get_parser(field)
 
-        importer = self.importer_handler.get_importer(field)
+        output_path = os.path.join(self.parent_directory, f"waze_{field}")
 
         return DF.Flow(
-            self.get_items(waze_data, field),
-            parser,
-            importer
+            self.get_items(waze_data, field), parser, DF.dump_to_path(output_path)
         )
