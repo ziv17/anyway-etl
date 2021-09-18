71dc0f9751ac54233329ed6216e0c94d68857fa7
Atalya Alon <20992625+atalyaalon@users.noreply.github.com>
2021-09-18 16:19:02 +0300

    update schedule interval
    ideally we'll check it daily and update our DB only upon new emails and load starting relevant years that arrived in email

--- airflow_server/dags/cbs.py
+++ airflow_server/dags/cbs.py
@@ -8,7 +8,7 @@ dag_kwargs = dict(
     default_args={
         'owner': 'airflow',
     },
-    schedule_interval='@daily',
+    schedule_interval='@weekly',
     catchup=False,
     start_date=days_ago(2),
 )
