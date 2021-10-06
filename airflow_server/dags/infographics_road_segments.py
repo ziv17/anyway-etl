bc1296f9e41ae32e55e595213b015e82516245d6
Atalya Alon <20992625+atalyaalon@users.noreply.github.com>
2021-10-06 11:16:34 +0300

    Create infographics_road_segments.py

---
+++ airflow_server/dags/infographics_road_segments.py
@@ -0,0 +1,21 @@
+from airflow import DAG
+from airflow.utils.dates import days_ago
+
+from anyway_etl_airflow.operators.cli_bash_operator import CliBashOperator
+
+
+dag_kwargs = dict(
+    default_args={
+        'owner': 'airflow',
+    },
+    schedule_interval=None,
+    catchup=False,
+    start_date=days_ago(2),
+)
+
+
+with DAG('fill-infographics-cache-for-road-segments', **dag_kwargs) as fill_infographics_cache_dag_for_road_segments:
+    CliBashOperator(
+        'anyway-etl anyway-kubectl-exec python3 main.py process infographics-data-cache-for-road-segments',
+        task_id='fill-infographics-cache-for-road-segments'
+    )
