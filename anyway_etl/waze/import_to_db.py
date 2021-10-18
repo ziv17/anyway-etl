d3786ff2710d8e4f59390a3eaf5d2978c25ef565
Carmel <16carmel@gmail.com>
2021-10-18 18:22:31 +0300

    Refactored waze import

--- anyway_etl/waze/import_to_db.py
+++ anyway_etl/waze/import_to_db.py
@@ -1,84 +1,93 @@
 import os
 import dataflows as DF
 import pandas as pd
+from datetime import datetime
+from typing import Callable
 from sqlalchemy.orm.session import Session
 
-from anyway_etl.waze.config import FIELDS, TYPES_MAPPING, UUID
+from anyway_etl.waze.config import FIELDS, TYPES_MAPPING, QUERIES
 from anyway_etl.db import session_decorator
 from anyway_etl.config import ANYWAY_ETL_DATA_ROOT_PATH
 
 
-def _get_saved_data(datapackage_filename) -> list:
-    return DF.Flow(DF.load(datapackage_filename)).results()[0][0]
+@session_decorator
+def __does_uuid_exist(session: Session, select_count_query: str, uuid: str) -> bool:
+    query_with_uuid = select_count_query.format(uuid=uuid)
+
+    db_execution = session.execute(query_with_uuid)
+
+    results = list(db_execution)
+
+    if len(results) == 0:
+        return False
+
+    count = results[0][0]
+
+    return count > 0
 
 
 @session_decorator
-def _import_data(session: Session, field: str, data: list):
-    data_type = TYPES_MAPPING[field]
-    session.bulk_insert_mappings(data_type, data)
+def __insert_to_db(session: Session, insert_query: str, row: dict) -> None:
+    row.pop("uuid", None)
+
+    insert_query_with_values = insert_query.format(**row)
+
+    session.execute(insert_query_with_values)
+
     session.commit()
 
 
 @session_decorator
-def _get_existing_data_insertion_times(
-    session: Session, field: str, data: list
-) -> dict:
-    data_type = TYPES_MAPPING[field]
+def __update_row(session: Session, update_query: str, row: dict) -> None:
+    row.pop("uuid", None)
 
-    uuids_from_waze_feed = [str(item.get(UUID)) for item in data]
+    row.pop("id", None)
 
-    uuids_from_waze_feed = list(set(uuids_from_waze_feed))
+    row.pop("insertion_time", None)
 
-    query = session.query(data_type).filter(
-        getattr(data_type, UUID).in_(uuids_from_waze_feed)
-    )
+    row["update_time"] = datetime.now()
 
-    data_from_db: pd.DataFrame = pd.read_sql_query(query.statement, query.session.bind)
+    update_query_with_values = update_query.format(**row)
 
-    uuid_to_insertion_time = {
-        row.uuid: row.insertion_time for row in data_from_db.itertuples()
-    }
-
-    session.query(data_type).filter(
-        getattr(data_type, UUID).in_(uuids_from_waze_feed)
-    ).delete(synchronize_session="fetch")
+    session.execute(update_query_with_values)
 
     session.commit()
 
-    return uuid_to_insertion_time
+
+def __get_row_handler(field: str) -> Callable[[dict], None]:
+    field_queries = QUERIES.get(field)
+
+    select_count_query = field_queries.get("select_count")
+
+    insert_query = field_queries.get("insert")
+
+    update_query = field_queries.get("update")
+
+    def _handler(row: dict) -> None:
+        uuid = row.get("uuid")
+        uuid_exists = __does_uuid_exist(select_count_query, uuid)
+
+        if uuid_exists:
+            __update_row(update_query, row)
+        else:
+            __insert_to_db(insert_query, row)
+
+    return _handler
 
 
-def _set_insertion_times(data: list, uuid_to_insertion_time: dict) -> list:
-    df = pd.DataFrame(data)
+def __get_insertion_flow(field: str) -> DF.Flow:
+    full_path = os.path.join(ANYWAY_ETL_DATA_ROOT_PATH, "waze", field)
 
-    df.set_index(UUID, inplace=True)
+    data_path = os.path.join(full_path, "datapackage.json")
 
-    df.index = df.index.map(str)
+    handler = __get_row_handler(field)
 
-    for uuid, insertion_time in uuid_to_insertion_time.items():
-        df.loc[uuid, "insertion_time"] = insertion_time
-
-    df.reset_index(inplace=True)
-
-    df.rename(columns={"index": UUID}, inplace=True)
-
-    return df.to_dict("records")
+    return DF.Flow(DF.load(path), handler)
 
 
 def import_waze_data_to_db():
     for field in FIELDS:
-        full_path = os.path.join(ANYWAY_ETL_DATA_ROOT_PATH, "waze", field)
 
-        data_path = os.path.join(full_path, "datapackage.json")
+        flow = __get_insertion_flow(field)
 
-        data = _get_saved_data(datapackage_filename=data_path)
-
-        existing_uuids_and_insertion_times = _get_existing_data_insertion_times(
-            field, data
-        )
-
-        data = _set_insertion_times(
-            data, uuid_to_insertion_time=existing_uuids_and_insertion_times
-        )
-
-        _import_data(field, data)
+        flow.process()
