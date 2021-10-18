a545ed6370c4aebe9f04182243ef432cae7b0b3b
Carmel <16carmel@gmail.com>
2021-10-18 18:21:56 +0300

    Updated config

--- anyway_etl/waze/config.py
+++ anyway_etl/waze/config.py
@@ -1,6 +1,6 @@
 from anyway.models import WazeAlert, WazeTrafficJams
 
-ISRAEL_POLYGON = "33.662000,32.556000;34.722000,33.004000;35.793000,33.370000;35.914000,32.953000;35.765000,32.733000;35.600000,32.628000;35.473000,31.073000;35.230000,30.290000;34.985000,29.513000;34.898000,29.483000;33.662000,32.556000"
+ISRAEL_POLYGON = "34.123,31.4;34.722,33.004;35.793,33.37;35.914,32.953;35.765,32.733;35.6,32.628;35.473,31.073;35.23,30.29;34.985,29.513;34.898,29.483;34.123,31.4"
 
 API_URL = "https://il-georss.waze.com/rtserver/web/TGeoRSS"
 
@@ -9,8 +9,6 @@ ALERTS = "alerts"
 JAMS = "jams"
 
 FIELDS = [ALERTS, JAMS]
-
-UUID = "uuid"
 
 
 API_PARAMS = {
@@ -46,3 +44,164 @@ COLUMNS_MAPPING = {
 }
 
 TYPES_MAPPING = {ALERTS: WazeAlert, JAMS: WazeTrafficJams}
+
+QUERIES = {
+    ALERTS: {
+        "select_count": """
+            SELECT COUNT(UUID)
+            FROM waze_alerts 
+            WHERE UUID = '{uuid}'
+        """,
+        "update": """
+            UPDATE waze_alerts
+            SET city='{city}',
+                confidence={confidence},
+                created_at='{created_at}',
+                latitude={latitude},
+                magvar={magvar},
+                number_thumbs_up={number_thumbs_up},
+                report_rating={report_rating},
+                reliability={reliability},
+                alert_type='{alert_type}',
+                alert_subtype='{alert_subtype}',
+                street='{street}',
+                road_type={road_type},
+                geom='{geom}',
+                jam_uuid='{jam_uuid}',
+                longitude={longitude},
+                report_by_municipality_user={report_by_municipality_user},
+                report_description='{report_description}',
+                ended_at_estimate='{ended_at_estimate}',
+                back_filled={back_filled},
+                update_time='{update_time}'
+            WHERE UUID='{uuid}'
+        """,
+        "insert": """
+            INSERT INTO waze_alerts (
+                city,    
+                confidence,
+                created_at,
+                latitude,
+                magvar,
+                number_thumbs_up,
+                report_rating,
+                reliability,
+                alert_type,
+                alert_subtype,
+                street,
+                road_type,
+                geom,
+                jam_uuid,
+                longitude,
+                report_by_municipality_user,
+                report_description,
+                ended_at_estimate,
+                back_filled,
+                update_time,
+                insertion_time
+            )
+            VALUES (
+                '{city}',
+                {confidence},
+                '{created_at}',
+                {latitude},
+                {magvar},
+                {number_thumbs_up},
+                {report_rating},
+                {reliability},
+                '{alert_type}',
+                '{alert_subtype}',
+                '{street}',
+                {road_type},
+                '{geom}',
+                '{jam_uuid}',
+                {longitude},
+                {report_by_municipality_user},
+                '{report_description}',
+                '{ended_at_estimate}',
+                {back_filled},
+                '{update_time}',
+                '{insertion_time}'
+            )
+        """,
+    },
+    JAMS: {
+        "select_count": """
+            SELECT COUNT(UUID)
+            FROM waze_traffic_jams 
+            WHERE UUID = '{uuid}'
+        """,
+        "update": """
+            UPDATE waze_traffic_jams
+            SET level={level},
+                line='{line}',
+                speed_kmh={speed_kmh},
+                turn_type={turn_type},
+                length={length},
+                type={type},
+                speed={speed},
+                segments='{segments}',
+                road_type={road_type},
+                delay={delay},
+                street='{street}',
+                city='{city}',
+                end_node='{end_node}',
+                blocking_alert_uuid='{blocking_alert_uuid}',
+                start_node='{start_node}',
+                created_at='{created_at}',
+                geom='{geom}',
+                ended_at_estimate='{ended_at_estimate}',
+                back_filled={back_filled},
+                update_time='{update_time}'
+            WHERE UUID='{uuid}'
+        """,
+        "insert": """
+            INSERT INTO waze_traffic_jams (
+                level,    
+                line,
+                speed_kmh,
+                turn_type,
+                length,
+                type,
+                speed,
+                segments,
+                road_type,
+                delay,
+                street,
+                city,
+                end_node,
+                blocking_alert_uuid,
+                start_node,
+                created_at,
+                geom,
+                ended_at_estimate,
+                back_filled,
+                update_time,
+                insertion_time
+            )
+            VALUES (
+                {level},
+                '{line}',
+                {speed_kmh},
+                {turn_type},
+                {length},
+                {type},
+                {speed},
+                '{segments}',
+                {road_type},
+                {delay},
+                '{street}',
+                '{city}',
+                '{end_node}',
+                '{blocking_alert_uuid}',
+                '{start_node}',
+                '{created_at}',
+                '{geom}',
+                '{ended_at_estimate}',
+                {back_filled},
+                '{update_time}',
+                '{insertion_time}'
+            )
+        """,
+    },
+}
