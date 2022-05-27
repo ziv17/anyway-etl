1fc60934bbdda2f3f73e067149d817025b627afd
Atalya Alon <20992625+atalyaalon@users.noreply.github.com>
2022-05-27 18:10:13 +0300

    Create test_logs.py

---
+++ airflow_server/dags/test_logs.py
@@ -0,0 +1,19 @@
+from airflow import DAG
+
+from anyway_etl_airflow.operators.cli_bash_operator import CliBashOperator
+
+
+dag_kwargs = dict(
+    default_args={
+        'owner': 'airflow',
+    },
+    schedule_interval=None,
+    catchup=False,
+)
+
+
+with DAG('test_logs', **dag_kwargs) as test_logs:
+    CliBashOperator(
+        'anyway-etl anyway-kubectl-exec python3 main.py process scripts test-airflow'
+        task_id='test_logs'
+    )
