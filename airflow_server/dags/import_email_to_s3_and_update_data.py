b5b0dff6fdeb6ce4c32dae54764357b63106568e
tkalir <tkalir@gmail.com>
2022-08-12 13:57:59 +0300

    changing import_email_to_s3_and_update_data to weekly (#30)

--- airflow_server/dags/import_email_to_s3_and_update_data.py
+++ airflow_server/dags/import_email_to_s3_and_update_data.py
@@ -8,7 +8,7 @@ dag_kwargs = dict(
     default_args={
         'owner': 'airflow',
     },
-    schedule_interval='@monthly',
+    schedule_interval='@weekly',
     catchup=False,
     start_date=days_ago(2),
 )
